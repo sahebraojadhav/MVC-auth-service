4f1d59bb6c8c93cf8d3ae719b281eced757f25b9
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-08-19 14:06:34 +0530

    created login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,52 @@
 import "./App.css";
 import FloatingShape from "./components/FloatingShape";
-import { Route,Routes } from "react-router-dom";
+import { Navigate, Route,Routes } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import {Toaster} from 'react-hot-toast'
+import { Children, useEffect } from "react";
+import { useAuthStore } from "./store/authStore";
+import DashboardPage from "./pages/DashboardPage";
+import LoadingSpinner from "./components/LoadingSpinner"
 
+//protect routes that require authentication
+const ProtectedRoutes=({children})=>{
+  const {isAuthenticated,user}=useAuthStore();
+
+  if(!isAuthenticated){
+    return <Navigate to="/login" replace/>
+  }
+
+  if(!user.isVerified){
+    return <Navigate to="/verify-path" replace/>
+  }
+
+  return children;
+}
+
+const RedirectAuthenticatedUser=({children})=>{
+  const {isAuthenticated,user}=useAuthStore();
+  
+  if(isAuthenticated && user.isVerified){
+    return <Navigate to="/" replace/>
+  }
+
+  return children;
+}
 
 function App() {
+  const {isCheckingAuth,checkAuth,isAuthenticated,user}=useAuthStore();
+ 
+ useEffect(()=>{
+  checkAuth()
+ },[checkAuth]);
+
+ console.log("isAuthenticated",isAuthenticated);
+ console.log("user",user);
+
+ if(isCheckingAuth) return <LoadingSpinner/>
+
   return (
     <div className="min-h-screen bg-gradient-to-br 
       from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden
@@ -16,11 +57,25 @@ function App() {
     
     <Routes>
       
-      <Route path="/" element={"Home"}/>
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-    </Routes>
+      <Route path="/" element={
+        <ProtectedRoutes>
+          <DashboardPage/>
+        </ProtectedRoutes>
+      }/>
+      <Route path="/signup" element={
+        <RedirectAuthenticatedUser>
+          <SignUpPage/>
+        </RedirectAuthenticatedUser>
+      }/>
+      <Route path="/login" element={      
+        <RedirectAuthenticatedUser>
+         <LoginPage/>
+      </RedirectAuthenticatedUser>
+      }/>
 
+      <Route path="/verify-path" element={<EmailVerificationPage/>}/>
+    </Routes>
+      <Toaster />
     </div>
   );
 }
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { User, Mail, Lock } from "lucide-react";
-import {Link} from 'react-router-dom'
+import { User, Mail, Lock, Loader } from "lucide-react";
+import {Link, useNavigate} from 'react-router-dom'
 import PasswordStrenghtMeter from "../components/PasswordStrengthMeter";
+import { useAuthStore } from "../store/authStore";
+import { Navigate } from "react-router-dom";
 
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const {signup,error,isLoading}=useAuthStore();
+  const navigate=useNavigate();
 
-  const handleSignup = (e) => {
+  const handleSignup = async(e) => {
     e.preventDefault();
+
+    try{
+      await signup(email,password,name);
+      navigate("/verify-path");
+    }catch(error){
+      console.log(error)
+    }
+
   };
 
   return ( 
@@ -47,10 +59,13 @@ const SignUpPage = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+
+          {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+
           <PasswordStrenghtMeter password={password}/>
 
-          <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-md hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200" whileHover={{scale:1.02}} whileTap={{scale:0.98}} type="submit">
-            signup
+          <motion.button className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-md hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200" whileHover={{scale:1.02}} whileTap={{scale:0.98}} type="submit" disabled={isLoading}>
+           {isLoading ? <Loader className="animate-spin mx-auto" size={24}/>:"Sign UP"}
           </motion.button>
         </form>
       </div>
